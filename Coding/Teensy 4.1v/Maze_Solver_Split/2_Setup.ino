e08eb02fbc79edb78167d5810c51af0408fc5976
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2023-07-08 02:45:42 +0530

    Add files via upload

---
+++ Coding/Teensy 4.1v/Maze_Solver_Split/2_Setup.ino
@@ -0,0 +1,74 @@
+void setup()
+{
+  Serial.begin(9600);
+  bt.begin(9600); // BT-MODULE (HC05)
+  pinMode(START_BUTTON, INPUT_PULLUP);
+
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+
+  pinMode(LEFT_PWM_PIN, OUTPUT);  // MOTOR DRIVER
+  pinMode(LEFT_IN1_PIN, OUTPUT);
+  pinMode(LEFT_IN2_PIN, OUTPUT);
+  pinMode(RIGHT_PWM_PIN, OUTPUT);
+  pinMode(RIGHT_IN1_PIN, OUTPUT);
+  pinMode(RIGHT_IN2_PIN, OUTPUT);
+  pinMode(STANDBY_PIN, OUTPUT);
+  pinMode(LeftTOF, OUTPUT);  // TOF
+  pinMode(CenterTOF, OUTPUT);
+  pinMode(RightTOF, OUTPUT);
+  digitalWrite(LeftTOF, LOW);
+  digitalWrite(CenterTOF, LOW);
+  digitalWrite(RightTOF, LOW);
+
+
+  attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_PIN_A), leftEncoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER_PIN_A), rightEncoderISR, CHANGE);
+
+  pidLeft.begin();
+  pidLeft.tune(LEFT_KP, LEFT_KI, LEFT_KD);
+  pidLeft.limit(40, 240);   //(30, 255);
+  pidRight.begin();
+  pidRight.tune(RIGHT_KP, RIGHT_KI, RIGHT_KD);
+  pidRight.limit(40, 240);  //(30, 255);
+
+  pid_Forward_Left.begin();
+  pid_Forward_Left.tune(Forward_LEFT_KP, Forward_LEFT_KI, Forward_LEFT_KD);
+  pid_Forward_Left.limit(30, 255);   //(30, 255);
+  pid_Forward_Right.begin();
+  pid_Forward_Right.tune(Forward_RIGHT_KP, Forward_RIGHT_KI, Forward_RIGHT_KD);
+  pid_Forward_Right.limit(30, 255);  //(30, 255);
+
+
+  Wire2.begin();
+  Serial.println("Assigning TOF Address");           // TOF ADDRESS SETUP
+  bt.println("Assigning TOF Address");
+  Serial.println("L");           // TOF ADDRESS SETUP
+  bt.println("L");
+  digitalWrite(LeftTOF, HIGH);
+  delay(150);
+  sensorL.setBus(&Wire2);
+  sensorL.init(true);
+  delay(100);
+  sensorL.setAddress((uint8_t)1);
+  Serial.println("C");
+  bt.println("C");
+  digitalWrite(CenterTOF, HIGH);
+  delay(150);
+  sensorC.setBus(&Wire2);
+  sensorC.init(true);
+  delay(100);
+  sensorC.setAddress((uint8_t)2);
+  Serial.println("R");
+  bt.println("R");
+  digitalWrite(RightTOF, HIGH);
+  delay(150);
+  sensorR.setBus(&Wire2);
+  sensorR.init(true);
+  delay(100);
+  sensorR.setAddress((uint8_t)3);
+  Serial.println("Done");
+  bt.println("Done");
+  sensorL.startContinuous();
+  sensorC.startContinuous();
+  sensorR.startContinuous();
+}
---
+++ Coding/Teensy 4.1v/Maze_Solver_Split/4_Wall.ino
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Left_Wall()
+{
+  int Left_Sensor_Reading = sensorL.readRangeContinuousMillimeters();
+  Serial.print("L: ");
+  Serial.println(Left_Sensor_Reading);
+  bt.print("L: ");
+  bt.println(Left_Sensor_Reading);
+  if (Left_Sensor_Reading > sensor_Lower_Limits[0] && Left_Sensor_Reading < sensor_Upper_Limits[0])
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Center_Wall()
+{
+  int Center_Sensor_Reading = sensorC.readRangeContinuousMillimeters();
+  Serial.print("C: ");
+  Serial.println(Center_Sensor_Reading);
+  bt.print("C: ");
+  bt.println(Center_Sensor_Reading);
+  if (Center_Sensor_Reading > sensor_Lower_Limits[1] && Center_Sensor_Reading < sensor_Upper_Limits[1])
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Right_Wall()
+{
+  int Right_Sensor_Reading = sensorR.readRangeContinuousMillimeters();
+  Serial.print("R: ");
+  Serial.println(Right_Sensor_Reading);
+  bt.print("R: ");
+  bt.println(Right_Sensor_Reading);
+  if (Right_Sensor_Reading > sensor_Lower_Limits[2] && Right_Sensor_Reading < sensor_Upper_Limits[2])
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
